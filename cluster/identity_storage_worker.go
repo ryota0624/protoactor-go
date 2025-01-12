6900f82d34443a1d54311349a6b6a905056a725a
ryota.suzuki <ryota.suzuki@10x.co.jp>
2025-01-12 21:14:59 +0900

    impl identity storage worker

--- cluster/identity_storage_worker.go
+++ cluster/identity_storage_worker.go
@@ -2,8 +2,9 @@ package cluster
 
 import (
 	"encoding/json"
-
 	"log"
+	"log/slog"
+	"time"
 
 	"github.com/asynkron/protoactor-go/actor"
 )
@@ -47,17 +48,78 @@ func (ids *IdentityStorageWorker) Receive(c actor.Context) {
 	activation := ids.storage.TryGetExistingActivation(getPid.ClusterIdentity)
 	if activation != nil {
 		pid := &actor.PID{}
-		go func() {
+		err := json.Unmarshal([]byte(activation.Pid), pid)
+		if err != nil {
+			panic(err)
+		}
+		if ids.cluster.MemberList.ContainsMemberID(activation.MemberID) {
+			ids.cluster.PidCache.Set(getPid.ClusterIdentity.Identity, getPid.ClusterIdentity.Kind, pid)
+			c.Respond(newPidResult(pid))
+			return
+		} else {
+			/// TODO: 1 memberID につき一度だけ発生させる
+			c.Logger().Info("MemberID not found in MemberList, removing activation")
+			ids.storage.RemoveMemberId(activation.MemberID)
+		}
+	}
+
+	activator := ids.cluster.MemberList.GetActivatorMember(getPid.ClusterIdentity.Identity, getPid.ClusterIdentity.Kind)
+
+	if activator == "" {
+		c.Respond(newPidResult(nil))
+		return
+	}
+
+	lock := ids.storage.TryAcquireLock(getPid.ClusterIdentity)
+	if lock == nil {
+		activation := ids.storage.WaitForActivation(getPid.ClusterIdentity)
+		if activation != nil {
+			pid := &actor.PID{}
 			err := json.Unmarshal([]byte(activation.Pid), pid)
 			if err != nil {
 				panic(err)
 			}
-			ids.cluster.PidCache.Set(getPid.ClusterIdentity.Identity, getPid.ClusterIdentity.Kind, pid)
-		}()
-
-		c.Respond(newPidResult(pid))
+			if ids.cluster.MemberList.ContainsMemberID(activation.MemberID) {
+				ids.cluster.PidCache.Set(getPid.ClusterIdentity.Identity, getPid.ClusterIdentity.Kind, pid)
+				c.Respond(newPidResult(pid))
+				return
+			} else {
+				/// TODO: 1 memberID につき一度だけ発生させる
+				c.Logger().Info("MemberID not found in MemberList, removing activation")
+				ids.storage.RemoveMemberId(activation.MemberID)
+			}
+		}
+		c.Respond(newPidResult(nil))
 		return
 	}
+	defer func() {
+		ids.storage.RemoveLock(*lock)
+	}()
 
+	pid := ids.SpawnActivation(activator, *lock)
+	c.Respond(newPidResult(pid))
 	return
+}
+
+func (ids *IdentityStorageWorker) SpawnActivation(activatorAddress string, lock SpawnLock) *actor.PID {
+	ids.cluster.Logger().Info("Spawning activation", slog.Any("spawnTo", activatorAddress), slog.Any("clusterIdentity", lock.ClusterIdentity))
+	remotePid := RemotePlacementActor(activatorAddress)
+	activateRequest := &ActivationRequest{
+		ClusterIdentity: lock.ClusterIdentity,
+		RequestId:       lock.LockID,
+	}
+
+	activationResult, err := ids.cluster.ActorSystem.Root.RequestFuture(remotePid, activateRequest, 5*time.Second).Result()
+	if err != nil {
+		ids.cluster.Logger().Error("Failed to activate", slog.Any("error", err))
+		return nil
+	}
+
+	activationResponse := activationResult.(*ActivationResponse)
+	if activationResponse.Failed {
+		ids.cluster.Logger().Error("Failed to activate")
+		return nil
+	}
+	ids.cluster.PidCache.Set(lock.ClusterIdentity.Identity, lock.ClusterIdentity.Kind, activationResponse.Pid)
+	return activationResponse.Pid
 }
