ab582910d1c9c90c48cc43c27b0fa8fa499f9f34
ryota.suzuki <ryota.suzuki@10x.co.jp>
2025-01-19 17:59:34 +0900

    persistenceが意図通り動かないパターンの実装

---
+++ examples/cluster-identitylookup-postgresql-persistence/node/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	console "github.com/asynkron/goconsole"
+	"github.com/asynkron/protoactor-go/cluster/clusterproviders/automanaged"
+	"github.com/asynkron/protoactor-go/persistence"
+	"time"
+
+	"cluster-identitylookup-postgresql-persistence/shared"
+
+	"github.com/asynkron/protoactor-go/actor"
+	"github.com/asynkron/protoactor-go/cluster"
+	"github.com/asynkron/protoactor-go/remote"
+)
+
+func main() {
+	c, clean := startNode(autoManagePorts[0])
+	defer clean()
+
+	_, clean2 := startNode(autoManagePorts[1])
+	defer clean2()
+
+	for {
+		if c.MemberList.Length() > 1 {
+			break
+		}
+		c.Logger().Info("Waiting for other nodes to join")
+		time.Sleep(time.Second)
+	}
+
+	pid := c.Get("abc", "hello")
+	fmt.Printf("Got pid %v\n", pid)
+	res, _ := c.Request("abc", "hello", &shared.HelloRequest{Name: "Roger"})
+	fmt.Printf("Got response %v\n", res)
+	fmt.Print("\nBoot other nodes and press Enter\n")
+	i := 0
+	for {
+		st, _ := console.ReadLine()
+		if st == "exit" {
+			break
+		}
+		messageToCluster(c, st, i)
+		i += 1
+	}
+
+	c.Shutdown(true)
+}
+
+func messageToCluster(c *cluster.Cluster, st string, i int) {
+	pid := c.Get(st, "hello")
+	c.Logger().Info("Got pid %v", pid)
+	res, _ := c.Request(st, "hello", &shared.HelloRequest{Name: fmt.Sprintf("message %v", i)})
+	c.Logger().Info("Got response %v", res)
+}
+
+var autoManagePorts = []int{
+	8081,
+	8082,
+	//8083,
+}
+
+var (
+	autoManageHosts []string
+)
+
+func init() {
+	for _, port := range autoManagePorts {
+		autoManageHosts = append(autoManageHosts, fmt.Sprintf("localhost:%v", port))
+	}
+}
+
+func startNode(autoManagePort int) (*cluster.Cluster, func()) {
+	system := actor.NewActorSystem()
+
+	provider := automanaged.NewWithConfig(time.Second*2, autoManagePort, autoManageHosts...)
+	lookup, persistenceProvider, clean := shared.NewLockUp(system)
+	config := remote.Configure("localhost", 0)
+
+	props := actor.PropsFromProducer(func() actor.Actor {
+		return &HelloActor{}
+	}, actor.WithReceiverMiddleware(
+		persistence.Using(persistenceProvider),
+	))
+	helloKind := cluster.NewKind("hello", props)
+	clusterConfig := cluster.Configure("my-cluster", provider, lookup, config, cluster.WithKinds(helloKind))
+	c := cluster.New(system, clusterConfig)
+
+	c.StartMember()
+
+	return c, clean
+}
+
+type HelloActor struct {
+	lastReceivedName string
+	persistence.Mixin
+}
+
+func (h *HelloActor) Receive(ctx actor.Context) {
+	switch msg := ctx.Message().(type) {
+	case *actor.Started:
+		fmt.Printf("Started %v", msg)
+		go func() {
+			time.Sleep(time.Second * 15)
+			ctx.Stop(ctx.Self())
+		}()
+	case *shared.HelloRequest:
+		h.lastReceivedName = msg.Name
+		if h.Recovering() {
+			fmt.Printf("Recovering %v\n", msg.Name)
+			return
+		}
+		h.PersistReceive(msg)
+		fmt.Printf("Hello %v\n", msg.Name)
+		ctx.Respond(&shared.HelloResponse{})
+	}
+}
